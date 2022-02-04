c58fe77d036a73044ea4f47b855b04253c6f7543
Lis <90345058+udd5564@users.noreply.github.com>
2022-02-03 19:51:34 -0500

    Create sketch.js

---
+++ 100Day_Challenges/sketch_100days_8/sketch.js
@@ -0,0 +1,136 @@
+
+let numSegments = 10;
+let direction = 'right';
+
+const xStart = 0; 
+const yStart = 250; 
+const diff = 10;
+
+let xCor = [];
+let yCor = [];
+
+let xFruit = 0;
+let yFruit = 0;
+let scoreElem;
+
+function setup() {
+  scoreElem = createDiv('Score = 0');
+  scoreElem.position(20, 20);
+  scoreElem.id = 'score';
+  scoreElem.style('color', 'white');
+
+  createCanvas(500, 500);
+  frameRate(15);
+  stroke(255);
+  strokeWeight(10);
+  updateFruitCoordinates();
+
+  for (let i = 0; i < numSegments; i++) {
+    xCor.push(xStart + i * diff);
+    yCor.push(yStart);
+  }
+}
+
+function draw() {
+  background(0);
+  for (let i = 0; i < numSegments - 1; i++) {
+    line(xCor[i], yCor[i], xCor[i + 1], yCor[i + 1]);
+  }
+  updateSnakeCoordinates();
+  checkGameStatus();
+  checkForFruit();
+}
+
+function updateSnakeCoordinates() {
+  for (let i = 0; i < numSegments - 1; i++) {
+    xCor[i] = xCor[i + 1];
+    yCor[i] = yCor[i + 1];
+  }
+  switch (direction) {
+    case 'right':
+      xCor[numSegments - 1] = xCor[numSegments - 2] + diff;
+      yCor[numSegments - 1] = yCor[numSegments - 2];
+      break;
+    case 'up':
+      xCor[numSegments - 1] = xCor[numSegments - 2];
+      yCor[numSegments - 1] = yCor[numSegments - 2] - diff;
+      break;
+    case 'left':
+      xCor[numSegments - 1] = xCor[numSegments - 2] - diff;
+      yCor[numSegments - 1] = yCor[numSegments - 2];
+      break;
+    case 'down':
+      xCor[numSegments - 1] = xCor[numSegments - 2];
+      yCor[numSegments - 1] = yCor[numSegments - 2] + diff;
+      break;
+  }
+}
+
+function checkGameStatus() {
+  if (
+    xCor[xCor.length - 1] > width ||
+    xCor[xCor.length - 1] < 0 ||
+    yCor[yCor.length - 1] > height ||
+    yCor[yCor.length - 1] < 0 ||
+    checkSnakeCollision()
+  ) {
+    noLoop();
+    const scoreVal = parseInt(scoreElem.html().substring(8));
+    scoreElem.html('Game ended! Your score was : ' + scoreVal);
+  }
+}
+
+
+function checkSnakeCollision() {
+  const snakeHeadX = xCor[xCor.length - 1];
+  const snakeHeadY = yCor[yCor.length - 1];
+  for (let i = 0; i < xCor.length - 1; i++) {
+    if (xCor[i] === snakeHeadX && yCor[i] === snakeHeadY) {
+      return true;
+    }
+  }
+}
+
+
+function checkForFruit() {
+  point(xFruit, yFruit);
+  if (xCor[xCor.length - 1] === xFruit && yCor[yCor.length - 1] === yFruit) {
+    const prevScore = parseInt(scoreElem.html().substring(8));
+    scoreElem.html('Score = ' + (prevScore + 1));
+    xCor.unshift(xCor[0]);
+    yCor.unshift(yCor[0]);
+    numSegments++;
+    updateFruitCoordinates();
+  }
+}
+
+function updateFruitCoordinates() {
+ 
+  xFruit = floor(random(10, (width - 100) / 10)) * 10;
+  yFruit = floor(random(10, (height - 100) / 10)) * 10;
+}
+
+function keyPressed() {
+  switch (keyCode) {
+    case 74:
+      if (direction !== 'right') {
+        direction = 'left';
+      }
+      break;
+    case 76:
+      if (direction !== 'left') {
+        direction = 'right';
+      }
+      break;
+    case 73:
+      if (direction !== 'down') {
+        direction = 'up';
+      }
+      break;
+    case 75:
+      if (direction !== 'up') {
+        direction = 'down';
+      }
+      break;
+  }
+}
